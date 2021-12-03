f26103f65f7f0e7f47bfbc1f9b02bc66fda1a1f4
Adwait Barkale <adwaitb@winjit.com>
2021-12-03 12:32:22 +0530

    Added Covid Details UI

--- Project Setup Demo/Architecture/MVP/Covid Test Module/View/CovidStatusViewController.swift
+++ Project Setup Demo/Architecture/MVP/Covid Test Module/View/CovidStatusViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 /// This class is controller for Covid Details View
 class CovidStatusViewController: BaseViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var tblViewCovidDetails: UITableView!
+    
     // MARK: - Variable Declaration
     let objCovidPresenter = CovidPresenter()
+    var arrLeftRightData = [LeftRightModel]()
     
     // MARK: - View's Initialization Methods
     override func viewWillAppear(_ animated: Bool) {
@@ -21,7 +25,8 @@ class CovidStatusViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+        configureTableView()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -40,6 +45,17 @@ class CovidStatusViewController: BaseViewController {
     
     // MARK: - User Defined Functions
 
+    func setupUI() {
+        
+    }
+    
+    /// Function Call to
+    func configureTableView() {
+        tblViewCovidDetails.register(UINib(nibName: "LeftRightCell", bundle: nil), forCellReuseIdentifier: "LeftRightCell")
+        tblViewCovidDetails.delegate = self
+        tblViewCovidDetails.dataSource = self
+        tblViewCovidDetails.separatorStyle = .none
+    }
 }
 
 // MARK: - Extensions
@@ -60,7 +76,19 @@ extension CovidStatusViewController: covidPresenterDelegate {
     /// Function call when Api call was successfully done
     /// - Parameter objCovid: Covid Details Object
     func getCovidStatus(objCovid: CovidDataModel) {
-        
+        arrLeftRightData.removeAll()
+        // Append New Data
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "Date", rightLabelData: "\(objCovid.date ?? 0)"))
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "States", rightLabelData: "\(objCovid.states ?? 0)"))
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "Positive Cases", rightLabelData: "\(objCovid.positive ?? 0)"))
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "Negative Cases", rightLabelData: "\(objCovid.negative ?? 0)"))
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "Pending Tests", rightLabelData: "\(objCovid.pending ?? 0)"))
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "Total Cases", rightLabelData: "\(objCovid.total ?? 0)"))
+        arrLeftRightData.append(LeftRightModel.createObject(leftLabelData: "Recovered Cases", rightLabelData: "\(objCovid.recovered ?? "Not Received")"))
+        // Update UI
+        DispatchQueue.main.async {
+            self.tblViewCovidDetails.reloadData()
+        }
     }
     
     /// Function call if any error occur during api calling or parsing
@@ -69,4 +97,24 @@ extension CovidStatusViewController: covidPresenterDelegate {
         self.showAlert(title: "", message: message)
     }
     
+}
+
+// MARK: - UITableViewDelegate UITableViewDataSource
+extension CovidStatusViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrLeftRightData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblViewCovidDetails.dequeueReusableCell(withIdentifier: "LeftRightCell", for: indexPath) as! LeftRightCell
+        let data = arrLeftRightData[indexPath.row]
+        cell.lblLeftLabel.text = data.leftData
+        cell.lblRightLabel.text = data.rightData
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 }
---
+++ Project Setup Demo/Resources/Common/Cells/LeftRightLabel/LeftRightModel.swift
@@ -0,0 +1,27 @@
+//
+//  LeftRightModel.swift
+//  Project Setup Demo
+//
+//  Created by Adwait Barkale on 03/12/21.
+//
+
+import Foundation
+
+/// This Class is used to hold Left Label Data and Right Label Data
+class LeftRightModel {
+    
+    var leftData = String()
+    var rightData = String()
+    
+    /// Function call when user need to create object of left right label data
+    /// - Parameters:
+    ///   - leftLabelData: Left Label
+    ///   - rightLabelData: Right Label
+    class func createObject(leftLabelData: String,rightLabelData: String) -> LeftRightModel {
+        let obj = LeftRightModel()
+        obj.leftData = leftLabelData
+        obj.rightData = rightLabelData
+        return obj
+    }
+    
+}
